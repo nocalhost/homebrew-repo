dfd5361464f424d44bdae51dc6f5bc6c07fbff48
王振威 <wangzhenwei@coding.net>
2020-11-09 15:44:47 +0800

    Update nocalhost.rb

--- Formula/nocalhost.rb
+++ Formula/nocalhost.rb
@@ -3,15 +3,15 @@ class Kapp < Formula
   homepage "https://nocalhost.dev"
   version "v0.0.1"
 
-  darwin_url = "TODO"
-  linux_x64_url = "TODO"
+  darwin_url = "https://codingcorp-generic.pkg.coding.net/nocalhost/nhctl/darwin/nhctl?version=${version}"
+  linux_x64_url = "https://codingcorp-generic.pkg.coding.net/nocalhost/nhctl/linux_x64/nhctl?version=${version}"
 
   if OS.mac?
     url darwin_url
     sha256 "a9c1a09c257cb94ac368916caa9c3092cf06297c6da82a0e55e570af8f04d33c"
   elsif OS.linux?
     url linux_x64_url
-    sha256 "a9c1a09c257cb94ac368916caa9c3092cf06297c6da82a0e55e570af8f04d33c"
+    sha256 "c69c71ce3ddb431117a7e2040914784a7888fca836f3ec33c72a94eac07b8cd0"
   end
 
   depends_on :arch => :x86_64
